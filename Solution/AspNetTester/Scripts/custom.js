d4af43ce98884bae662c91f8e5e621e51de14464
yana permana <webdev.sale@gmail.com>
2015-04-14 06:05:14 +0700

    Update 2.2 Finishing Loader Page - Edit _Layout.cshtml (add markup for loader), Edit custom.js (add script for first page loading)

--- Solution/AspNetTester/Scripts/custom.js
+++ Solution/AspNetTester/Scripts/custom.js
@@ -1,14 +1,21 @@
 $(window).on('load',function(){
 	
-	$('.slider').fractionSlider({
-		'fullWidth': 			true,
-		'controls': 			true, 
-		'pager': 				true,
-		'responsive': 			true,
-		'dimensions': 			"1000,480",
-	    'increase': 			false,
-		'pauseOnHover': 		false,
-		'slideEndAnimation': 	true
-	});
+    $('#loader-wrapper').fadeOut('slow', function () {
+        $('#page-wrapper').fadeIn('slow');
+        function winHeight(el) {
+            $(el).css('height', $(window).height());
+        }
+
+        $('.slider').fractionSlider({
+            'fullWidth': true,
+            'controls': true,
+            'pager': true,
+            'responsive': true,
+            'dimensions': "1000,480",
+            'increase': false,
+            'pauseOnHover': false,
+            'slideEndAnimation': true
+        });
+    });
 
 });
