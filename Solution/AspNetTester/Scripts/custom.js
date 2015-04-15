a4cea042377e71fc7018d88336f5876b78b47173
yana permana <webdev.sale@gmail.com>
2015-04-16 03:51:57 +0700

    Update 5.0 Update Index.cshtml, add static form, update Custom.js (add sidemenu script).

--- Solution/AspNetTester/Scripts/custom.js
+++ Solution/AspNetTester/Scripts/custom.js
@@ -1,17 +1,69 @@
 $(window).on('load',function(){
 	
-    // Hide loader and display content
-    $('#loader-wrapper').fadeOut('slow', function () {
+	// display sidemenu
+	$('.hide-menu').on('click',function(e){
+		e.preventDefault();
+		$('.side-hide').stop().animate({
+			right: '-240px'
+		},{
+			duration: 300,
+			complete: function(){
+				$('.show-menu').fadeIn();
+			}
+		}).removeClass('side-hide');
+	});
+	$('.show-menu').on('click',function(e){
+		e.preventDefault();
+		$('.sidemenu').stop().animate({
+			right: '0px'
+		},{
+			duration: 300,
+			complete: function(){
+				$('.show-menu').fadeOut();
+			}
+		}).addClass('side-hide');
+	});
+	$(window).on('scroll', function() {
+		var scrollTop = $(this).scrollTop();
+
+		$('.white-bg').each(function() {
+			var topDistance = $(this).offset().top;
+
+			if ( (topDistance+100) < scrollTop ) {
+				$('.side-hide').stop().animate({
+					right: '0px'
+				},{
+					duration: 300,
+					complete: function(){
+						$('.show-menu').fadeOut();
+					}
+				});
+			} else {
+				$('.side-hide').stop().animate({
+					right: '-190px'
+				},{
+					duration: 300,
+					complete: function(){
+						$('.show-menu').fadeIn();
+					}
+				});
+			}
+		});
+	});
+	// display sidemenu end
+	
+	// Hide loader and display content
+    $('#loader-wrapper').fadeOut('fast', function () {
         $('.slider').fractionSlider({
-            'fullWidth': true,
+            'fullWidth': false,
             'controls': true,
             'pager': true,
             'responsive': true,
-            'dimensions': "1000,480",
-            'increase': false,
+            'dimensions': "1440,580",
+            'increase': true,
             'pauseOnHover': false,
             'slideEndAnimation': true
         });
     });
-	
+
 });
